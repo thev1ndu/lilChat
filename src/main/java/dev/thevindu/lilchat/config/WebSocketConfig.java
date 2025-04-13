4dc946e484f532ea9e76c9f1e1ed6c4d80e3e371
ThevinduW <itsthw9@gmail.com>
2025-04-13 14:59:40 +0530

    Registry Endpoint, Origin, SockJS for fallback options

--- src/main/java/dev/thevindu/lilchat/config/WebSocketConfig.java
+++ src/main/java/dev/thevindu/lilchat/config/WebSocketConfig.java
@@ -15,11 +15,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:5172").withSockJS();
     }
 }
