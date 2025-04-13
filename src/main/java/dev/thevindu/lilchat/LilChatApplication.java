b28a1fe5f91710f8daf689d278c44ad0161fd777
ThevinduW <itsthw9@gmail.com>
2025-04-13 07:32:18 +0530

    Template

---
+++ src/main/java/dev/thevindu/lilchat/LilChatApplication.java
@@ -0,0 +1,13 @@
+package dev.thevindu.lilchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LilChatApplication.class, args);
+    }
+
+}
