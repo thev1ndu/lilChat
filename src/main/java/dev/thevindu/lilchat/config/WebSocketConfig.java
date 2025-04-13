79c517686de6cca2a96093407bec2cb9b722ed1a
ThevinduW <itsthw9@gmail.com>
2025-04-13 14:52:27 +0530

    Message Broker Override Methods

--- src/main/java/dev/thevindu/lilchat/config/WebSocketConfig.java
+++ src/main/java/dev/thevindu/lilchat/config/WebSocketConfig.java
@@ -1,9 +1,25 @@
 package dev.thevindu.lilchat.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 // WebScoketMessageBrokerConfigurer: Two-Way Channel between Client & Server
 // STOMP: Structure to messages that passes to server. Does organization work
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+// Message broker used to direct the messages to the right place
 
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+    }
 }
