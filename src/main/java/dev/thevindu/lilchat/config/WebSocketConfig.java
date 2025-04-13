4f95f6c083c0cbd8cbc28451c3c52f5e995326d4
ThevinduW <itsthw9@gmail.com>
2025-04-13 14:21:41 +0530

    Init Commit

---
+++ src/main/java/dev/thevindu/lilchat/config/WebSocketConfig.java
@@ -0,0 +1,9 @@
+package dev.thevindu.lilchat.config;
+
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+// WebScoketMessageBrokerConfigurer: Two-Way Channel between Client & Server
+// STOMP: Structure to messages that passes to server. Does organization work
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+}
